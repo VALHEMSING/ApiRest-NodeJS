cbe0d6d56d50939d8fdeaf29df70a820bcca747c
Julian David <juli.rivera.quintero@gmail.com>
2024-09-04 17:28:30 -0500

    feat(certify):Add certify and refacthorizen

--- node-js-lab-server/index.js
+++ node-js-lab-server/index.js
@@ -1,3 +1,7 @@
+const https = require ('https');
+const fs = require ('fs');
+const path = require ('path');  
+
 // Traemos el archivo dotenv
 require('dotenv').config();
 
@@ -23,11 +27,13 @@ mongoose.connect(CONEXION)
     .then(() => console.log('Conectado a MongoDB'))
     .catch(err => console.error('No se pudo conectar a MongoDB', err));
 
+
+
 const app = express();
 
 // Configurar CORS de manera avanzada
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://anotherdomain.com'], // Reemplaza con los dominios permitidos
+  origin: ['https://localhost:3000', 'https://anotherdomain.com'], // Reemplaza con los dominios permitidos
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
   allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
   credentials: true, // Permite el envío de cookies
@@ -37,7 +43,13 @@ const corsOptions = {
 
 
 // Configurar CORS
-app.use(cors(corsOptions)); // Esto habilita CORS para todas las rutas
+app.use(cors(corsOptions)); // Esto habilita CORS para todas las ruta
+
+//Cargar certificador SSL y la clave privada
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'ssl','server.key')),
+  cert: fs.readFileSync(path.join(__dirname,'ssl','server.crt')),
+} 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -50,6 +62,13 @@ app.use('/api/usuarios', usuarioRouter);
 app.use('/api/cursos', cursosRouter);
 
 const port = process.env.PORT || 3000;
+/*
 app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+    console.log(`Servidor escuchando en https://localhost:${port}`);
+    console.log()
 });
+*/
+https.createServer(options,app).listen(port, ()=>{
+  console.log(`Servidor HTTPS escuchando en https://localhost:${port}`);
+    console.log(`API REST ejecutandose correctamente  `)
+})
